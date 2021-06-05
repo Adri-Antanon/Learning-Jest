6672cf018b623d4ffec6f92ca99bc7f823c5578d
Adrian Antanon <adrianantanon@gmail.com>
2021-06-05 11:45:50 +0200

    Async and promise tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,29 @@ test('Callback test', () => {
     expect(str).toBe('aloH');
   });
 });
+
+const reverseStringPromise = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error('No hay string'));
+    }
+    resolve(str.split("").reverse().join(""));
+  });
+};
+
+test('Promise test', () => {
+  return reverseStringPromise('Hola')
+    .then(str => {
+      expect(str).toBe('aloH');
+    });
+});
+
+test('Async Test', async () => {
+  const string = await reverseStringPromise('Hola');
+  expect(string).toBe('aloH');
+});
+
+afterEach(() => console.log('After each test'));
+afterAll(() => console.log('After all tests'));
+beforeEach(() => console.log('After each test'));
+beforeAll(() => console.log('After all tests'));
