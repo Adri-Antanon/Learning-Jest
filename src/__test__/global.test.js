07d2877659968b558d7e9521dccd4523969ccba0
Adrian Antanon <adrianantanon@gmail.com>
2021-06-05 11:34:57 +0200

    implementation test for boolean, number, array and callback

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,28 @@
 const text = "Hola Mundo";
+const fruits = ["Manzana", "Melón", "Banana"];
 
 test('It must contain a text', () => {
   expect(text).toMatch(/Mundo/);
+});
+
+test('Is there a banana?', () => {
+  expect(fruits).toContain('Banana');
+});
+
+test('Greater than', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('Is true?', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, cb) => {
+  cb(str.split("").reverse().join(""));
+};
+
+test('Callback test', () => {
+  reverseString("Hola", (str) => {
+    expect(str).toBe('aloH');
+  });
 });
